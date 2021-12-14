d5a1bd52d364045d8601e3dc1d7ca6e2cbfac9bd
PrecisionApps <73882757+PrecisionApps@users.noreply.github.com>
2021-12-14 23:14:51 +0300

    Add files via upload

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "Dictionary-KaanOzge",
+    name: "Dictionary",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "Dictionary-KaanOzge",
-            targets: ["Dictionary-KaanOzge"]),
+            name: "Dictionary",
+            targets: ["Dictionary"]),
     ],
     dependencies: [
         .package(name: "Math", url: "https://github.com/StarlangSoftware/Math-Swift.git", .exact("1.0.3")),
@@ -18,15 +18,15 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "Dictionary-KaanOzge",
+            name: "Dictionary",
             dependencies: ["Math"],
             resources: [
-            .copy("mixedcase.txt"),
-            .copy("lowercase.txt"),
-            .copy("turkish_dictionary.txt"),
-            .copy("turkish_misspellings.txt")]),
+            .process("mixedcase.txt"),.process("lowercase.txt"),
+            .process("turkish_dictionary.txt"),
+            .process("turkish_misspellings.txt")
+            ]),
         .testTarget(
             name: "DictionaryTests",
-            dependencies: ["Dictionary-KaanOzge"]),
+            dependencies: ["Dictionary"]),
     ]
 )
